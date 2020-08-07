5922d608586637546429dd8a876e9634e835e054
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-07 11:56:07 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,15 @@
-forever(function () {
-	
-})
+//% weight=100 color=#0fbc11 icon=“”
+
+namespace centrifuge {
+
+//% block
+
+export function servo(mypin:PwmOnlyPin ,value: number): void
+
+{
+
+mypin.servoWrite(value)
+
+}
+
+}
