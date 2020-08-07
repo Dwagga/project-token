a6d265c9f2891e4604f00e2e9920c4128fad23ac
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-07 12:00:57 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,20 +2,16 @@
 
 namespace centrifuge {
 
-//% block
+//% help=pins/servo-write-pin weight=20
+//% blockId=device_set_servo_pin block="servo write|pin %mypin|to %value" blockGap=8
+//% parts=microservo trackArgs=0
+//% value.min=0 value.max=180
+//% mypin.fieldEditor="gridpicker" mypin.fieldOptions.columns=6
+//% mypin.fieldOptions.tooltips="false" mypin.fieldOptions.width="250"
+//% weight=100 color=#0fbc11
 export function servo(mypin:PwmOnlyPin ,value: number): void {
 
   mypin.servoWrite(value)
 }
 
-//% help=pins/servo-write-pin weight=20
-//% blockId=device_set_servo_pin block="servo write|pin %name|to %value" blockGap=8
-//% parts=microservo trackArgs=0
-//% value.min=0 value.max=180
-//% name.fieldEditor="gridpicker" name.fieldOptions.columns=6
-//% name.fieldOptions.tooltips="false" name.fieldOptions.width="250"
-//% weight=100 color=#0fbc11
-export function servo2(name:number ,value: number): void {
-    pins.servoWritePin(name, value)
-}
 }
