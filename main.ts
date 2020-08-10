a453d80e4d6929fb67e2ebb8c87341c2c7401608
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-10 14:24:50 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -22,7 +22,7 @@ export function servo(mypin:PwmOnlyPin ,value: number): void {
 //% block="Start the centrifuge" blockGap=8
 //% weight=100 color=#FFA533
 export function Start_centrifuge(): void {
-  PwmOnlyPin pin = "D13"
+  PwmOnlyPin pin = "D13";
   pin.servoWrite(150)
   pause(7000)
 }
