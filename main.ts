d4c56789a6a8c820f5c5efe63e5385abb6de965c
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-10 14:16:39 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,19 +12,17 @@ function pause(ms: number): void {
 //% value.min=0 value.max=180
 //% mypin.fieldEditor="gridpicker" mypin.fieldOptions.columns=6
 //% mypin.fieldOptions.tooltips="false" mypin.fieldOptions.width="250"
-//% weight=100 color=#0fbc11
+//% weight=100 color=#FF5733
 export function servo(mypin:PwmOnlyPin ,value: number): void {
 
   mypin.servoWrite(value)
 }
   
 
-//% block="Start the centrifuge || pin: %mypin2 value:%value2" blockGap=8
-//% mypin2.deft="D13"
-//% value2.deft=150
-//% weight=100 color=#0fbc11
-export function Start_centrifuge(mypin2:PwmOnlyPin ,value2: number): void {
-  mypin2.servoWrite(value2)
+//% block="Start the centrifuge" blockGap=8
+//% weight=100 color=#FFA533
+export function Start_centrifuge(): void {
+  D13.servoWrite(150)
   pause(7000)
 }
   
