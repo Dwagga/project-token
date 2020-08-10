036be7c7ce334f223c9a080a099e90c17a967795
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-10 13:11:55 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 //% weight=100 color=#0fbc11 icon=“”
 
 namespace centrifuge {
+  
+//% help=loops/pause weight=99
+function pause(ms: number): void {
+    loops.pause(ms);
+}
 
 //% help=pins/servo-write-pin weight=20
 //% block="Centrifugue: servo write|pin %mypin|to %value" blockGap=8
@@ -13,6 +18,17 @@ export function servo(mypin:PwmOnlyPin ,value: number): void {
 
   mypin.servoWrite(value)
 }
-
+  
+//% help=pins/servo-write-pin weight=20
+//% block="Start the centrifuge" blockGap=8
+//% parts=microservo trackArgs=0
+//% mypin="D13"
+//% value=150
+//% weight=100 color=#0fbc11
+export function servo(mypin:PwmOnlyPin ,value: number): void {
+  mypin.servoWrite(value)
+  pause(7000)
+}
+  
 }
 
