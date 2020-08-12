89dc8c8edf5ba1d6d03ec78882e8d5257d14abb4
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-12 02:10:29 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,20 +1,25 @@
 //% weight=100 color=#FF5733 icon=\2707
 namespace centrifuge {
   
+enum potency {
+    100,
+    120,
+    140
+}
+  
 //% help=loops/pause weight=99
 function pause(ms: number): void {
     loops.pause(ms);
 }
 
-//% block="Centrifugue: servo write|pin %mypin|to %value" blockGap=8
+//% block="Centrifugue at |pin %mypin|to %value" blockGap=8
 //% value.min=0 value.max=180
-//% mypin.defl=D13
 //% value.defl=160
 //% mypin.fieldEditor="gridpicker" mypin.fieldOptions.columns=6
 //% mypin.fieldOptions.tooltips="false" mypin.fieldOptions.width="250"
 //% weight=100 color=#FF5733
-export function dwagga(value: number): void {
-  DAL.PA23.servoWrite(value)
+export function dwagga(value: potency): void {
+  mypin.servoWrite(value)
 }
   
 /*
