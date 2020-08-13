235d2822c748187ed08e1004e71b3c46601f30b3
Dwagga <43897652+Dwagga@users.noreply.github.com>
2020-08-14 00:22:08 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -15,9 +15,9 @@ function pause(ms: number): void {
 
 //% block="Run the centrifugue at %value during %duration" blockGap=8
 //% duration.fieldEditor="numberdropdown" value.fieldOptions.decompileLiterals=true
-//% duration.fieldOptions.values='[["5 seconds"], ["10 seconds"], ["30 seconds"], ["1 minute"]]'
+//% duration.fieldOptions.data='[["5 seconds"], ["10 seconds"], ["30 seconds"], ["1 minute"]]'
 //% value.fieldEditor="numberdropdown" value.fieldOptions.decompileLiterals=true
-//% value.fieldOptions.values='[["3.000 RPM"], ["6.000 RPM"], ["10.000 RPM"], ["14.000 RPM"]]'
+//% value.fieldOptions.data='[["3.000 RPM"], ["6.000 RPM"], ["10.000 RPM"], ["14.000 RPM"]]'
 //% weight=100 color=#FF5733
 export function run_centrifuge(value: string, duration: string): void {
   //pins.D13.servoWrite(value)
